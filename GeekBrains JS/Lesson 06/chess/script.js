d1391d77b76c8d7a3c8e9fe127c072ff28d88468
intrening <wol87@ya.ru>
2019-04-13 21:05:35 +0300

    lesson 06 50%

---
+++ GeekBrains JS/Lesson 06/chess/script.js
@@ -0,0 +1,76 @@
+var cells = [
+    ['R_B','N_B','B_B','Q_B','K_B','B_B','N_B','R_B'],
+    ['P_B','P_B','P_B','P_B','P_B','P_B','P_B','P_B'],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['P_W','P_W','P_W','P_W','P_W','P_W','P_W','P_W'],
+    ['R_W','N_W','B_W','Q_W','K_W','B_W','N_W','R_W']
+];
+
+renderCells();
+
+
+
+
+
+function renderCells(){
+    let line;
+    let cell;
+    createLineLetters (cells[0].length);
+    for (let i = 0; i < cells.length; i ++){
+        line = createLine();
+
+        cell = createCell('name',i+1);
+        line.appendChild(cell);
+        
+        for (let j = 0; j < cells[i].length; j++){
+            cell = createCell(cells[i][j],'',(j+i) % 2 ==0? 'white':'black');
+            line.appendChild(cell);
+        }
+    }
+
+    function createLineLetters(size_cells){
+        let line = createLine();
+        cell = createCell('name','')
+        line.appendChild(cell);
+    
+        for (let i = 0; i < size_cells; i++){
+            cell = createCell('name',String.fromCharCode(65+i));
+            line.appendChild(cell);
+        }
+    }
+    
+    function createLine(){
+        let line = document.createElement('div');
+        line.classList.add ('line');
+        
+        let cells = document.getElementById('cells');
+        cells.appendChild(line);
+        return line;
+    }
+    
+    function createCell (type, value, color){
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+    
+        if (color == 'white'){
+            cell.classList.add('white');
+        } else if (color == 'black'){
+            cell.classList.add('black');
+        }
+    
+        if (type == "name"){
+            cell.classList.add('name');
+            cell.innerHTML = value;
+        } else if (type != ''){
+            var figure = document.createElement('img');
+            figure.classList.add(type);
+            cell.appendChild(figure);
+        }
+    
+        return cell;
+    }
+}
+
